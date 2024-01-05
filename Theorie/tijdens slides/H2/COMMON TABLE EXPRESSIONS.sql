5f7620390cc49c30e973d9dfc35789121d10024d
Bauke Blomme <bauke.blomme@ugent.be>
2024-01-05 22:23:33 +0100

    update on cte in slides

--- Theorie/tijdens slides/H2/COMMON TABLE EXPRESSIONS.sql
+++ Theorie/tijdens slides/H2/COMMON TABLE EXPRESSIONS.sql
@@ -1,4 +1,6 @@
 ----- the WITH component -----
+-- one with
+
 -- naam geven aan de subquery, basically same als de join die ik eerst gebruikte
 -- handig om te hergebruiken wel
 WITH CategoryMinPrice(CategoryID, MinPrice)
@@ -8,3 +10,25 @@ WITH CategoryMinPrice(CategoryID, MinPrice)
 SELECT c.CategoryID, p.ProductID, MinPrice
 FROM Products AS p
          JOIN CategoryMinPrice AS c ON p.CategoryID = c.CategoryID AND p.UnitPrice = c.MinPrice;
+
+
+-- more than 1 WITH - component, geen meerder with's, gewoon meerere as's
+WITH TotalRevenuePerYear(RevenueYear, TotalRevenue)
+    AS
+    (SELECT YEAR (OrderDate), SUM (od.UnitPrice * od.Quantity)
+    FROM Orders o INNER JOIN OrderDetails od
+    ON o.OrderID = od.OrderID
+    GROUP BY YEAR (OrderDate)), TotalRevenuePerYearPerCustomer(RevenueYear, CustomerID, Revenue)
+    AS
+    (SELECT YEAR (OrderDate), o.CustomerID, SUM (od.UnitPrice * od.Quantity)
+    FROM Orders o INNER JOIN OrderDetails od
+    ON o.OrderID = od.OrderID
+    GROUP BY YEAR (OrderDate), o.CustomerID)
+SELECT CustomerID, pc.RevenueYear, FORMAT(Revenue / TotalRevenue, 'P') As RelativePart
+FROM TotalRevenuePerYearPerCustomer pc
+         INNER JOIN TotalRevenuePerYear t
+                    ON pc.RevenueYear = t.RevenueYear
+ORDER BY 2, 3 DESC
+
+
+-- Recursive SELECT's
